bde09fbe2b32c3093495fc0c3b0034e5c97c8238
Julian <julianf@volan.top>
2026-04-24 17:48:35 -0400

    add more presets

--- src/utils/notePresets.ts
+++ src/utils/notePresets.ts
@@ -11,11 +11,15 @@ export const noteStylePresets: NoteStylePreset[] = [
   { color: "yellow", width: 240, height: 220, decoration: "none" },
   { color: "blue", width: 180, height: 170, decoration: "pin" },
   { color: "pink", width: 160, height: 150, decoration: "none" },
-  { color: "green", width: 145, height: 130, decoration: "none" },
-  { color: "purple", width: 150, height: 140, decoration: "none" },
+  { color: "green", width: 145, height: 130, decoration: "pin" },
+  { color: "purple", width: 150, height: 140, decoration: "pin" },
   { color: "white", width: 245, height: 260, decoration: "tape" },
   { color: "grid-pink", width: 230, height: 220, decoration: "tape" },
   { color: "grid-white", width: 150, height: 145, decoration: "tape" },
+  { color: "yellow", width: 220, height: 190, decoration: "pin" },
+  { color: "blue", width: 205, height: 190, decoration: "none" },
+  { color: "pink", width: 210, height: 200, decoration: "tape" },
+  { color: "white", width: 190, height: 170, decoration: "none" },
 ];
 
 export function randomNotePreset() {
